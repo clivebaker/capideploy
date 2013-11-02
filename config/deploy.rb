9d41b5aaba6d53f5254f4f9eedbee2048237b119
Clive Baker <clive@clivebaker.com>
2013-11-02 15:54:27 +0000

    include bundler

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :repo_url, 'git@github.com:clivebaker/capideploy.git'
 
  set :linked_files, %w{config/database.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :linked_dirs, %w{log tmp public/system}
+set :linked_dirs, %w{bin log tmp public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
  set :keep_releases, 5
